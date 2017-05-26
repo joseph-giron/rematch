a617f286079a5fa328a656e41ab7aed499b5f794
Nir Izraeli <nirizr@gmail.com>
2017-05-25 17:04:48 -0700

    Add silent dialog
    and use it in autoupload
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/dialogs/__init__.py
+++ idaplugin/rematch/dialogs/__init__.py
@@ -1,11 +1,12 @@
 import base
 import widgets
 import gui
+import silent
 import login
 import match
 import project
 import settings
 import serializedgraph
 
-__all__ = ['base', 'widgets', 'gui', 'login', 'match', 'project', 'settings',
-           'serializedgraph']
+__all__ = ['base', 'widgets', 'gui', 'silent', 'login', 'match', 'project',
+           'settings', 'serializedgraph']
