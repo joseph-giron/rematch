207b0640d0c96ea78f8d3eaafc6a32898810de4e
shift <sr@sr.re>
2017-05-30 10:14:06 -0400

    In case our web container goes up faster than our DB, we should wait until we can successfully connect. This happens mostly during the first time we initialize docker-compose for the first run. Originally, Docker Compose offers a healthcheck field in docker-compose.yml however it doesn't seem to work properly and forces one to move to a newer docker-compose.yml vesrion (2.1)
    Having tried the original solution of nc'ating to 3306 and waiting until it is open works.
    
    p.s
    this is exactly what the healthcheck field does, however for some reason it doesn't work properly,
    may it be DNS, or some networking issues between the WEB container and the DB container.

--- server/start_celery.sh
+++ server/start_celery.sh
@@ -3,6 +3,7 @@
 if [[ -n "$IN_DOCKER" ]];
 then
   echo "starting celeryin docker mode!"
+  while ! nc -z db 3306 ; do sleep 2; done
   celery -A rematch.celery_docker worker -l info
 else
   echo "starting celeryin debug mode!"
