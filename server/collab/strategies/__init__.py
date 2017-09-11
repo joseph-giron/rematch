cf467cadae82fe48fa25da3232ddfb87be1ad8dd
Nir Izraeli <nirizr@gmail.com>
2017-09-12 01:51:35 +0300

    Fix code review issues
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/collab/strategies/__init__.py
+++ server/collab/strategies/__init__.py
@@ -1,10 +1,13 @@
 from .strategy import Strategy
 from .all_strategy import AllStrategy
 
+
 strategies_list = [AllStrategy]
+
 
 def strategy_choices():
   return [(s.strategy_type, s.strategy_name) for s in strategies_list
             if not s.is_abstract()]
+
 
 __all__ = ['Strategy', 'AllStrategy', 'strategies_list']
--- server/collab/strategies/strategy.py
+++ server/collab/strategies/strategy.py
@@ -1,7 +1,6 @@
 class Strategy(object):
   @classmethod
   def is_abstract(cls):
-    return False
     return not (hasattr(cls, 'strategy_type') and
                 hasattr(cls, 'strategy_description') and
                 hasattr(cls, 'strategy_name'))
