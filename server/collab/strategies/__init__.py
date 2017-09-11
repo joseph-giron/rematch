11d7afa4ea4362e72d1b68b78246af9e327678f9
Nir Izraeli <nirizr@gmail.com>
2017-09-11 19:40:13 +0300

    Add strategy field to task
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/collab/strategies/__init__.py
+++ server/collab/strategies/__init__.py
@@ -3,4 +3,8 @@ from .all_strategy import AllStrategy
 
 strategies_list = [AllStrategy]
 
+def strategy_choices():
+  return [(s.strategy_type, s.strategy_name) for s in strategies_list
+            if not s.is_abstract()]
+
 __all__ = ['Strategy', 'AllStrategy', 'strategies_list']
