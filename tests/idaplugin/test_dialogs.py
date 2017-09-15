375d86ac14dcc7115d997aa9bdd68ed1e128f61c
Nir Izraeli <nirizr@gmail.com>
2017-09-15 18:45:43 +0200

    Mark all dialog tests known to fail as xfail for now
    * also increase coverage
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- tests/idaplugin/test_dialogs.py
+++ tests/idaplugin/test_dialogs.py
@@ -17,11 +17,16 @@ dialogs = recurse_subclasses({idaplugin.rematch.dialogs.base.BaseDialog})
 dialogs = sorted(dialogs)
 
 
+known_failing_dialogs = ('MatchDialog', 'AddFileDialog', 'MatchResultDialog')
 @pytest.mark.parametrize("dialog_entry", dialogs)
 def test_dialog(dialog_entry, idapro_app):
   try:
     dialog = dialog_entry()
     dialog.show()
     idapro_app.processEvents()
-  except TypeError as ex:
-    pytest.skip(str(ex))
+  except Exception as ex:
+    if dialog_entry.__name__ in known_failing_dialogs:
+      pytest.xfail("Dialog {} which was expected to fail failed with "
+                   "exception {}".format(dialog_entry.__name__, str(ex)))
+    else:
+      raise
