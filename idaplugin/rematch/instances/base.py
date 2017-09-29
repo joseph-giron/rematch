4d031c481f08e50937f8a8d304958d5f5fa4dd93
Nir Izraeli <nirizr@gmail.com>
2017-09-29 13:44:02 -0700

    Add size when collecting instances
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/instances/base.py
+++ idaplugin/rematch/instances/base.py
@@ -8,10 +8,14 @@ class BaseInstance(object):
     self.vectors = {collectors.vectors.NameHashVector}
     self.annotations = {collectors.annotations.NameAnnotation}
 
+  def size(self):
+    return 0
+
   def serialize(self):
     vectors = list(collectors.collect(self.vectors, self.offset))
     annotations = list(collectors.collect(self.annotations, self.offset))
+    size = self.size()
 
     return {"file_version": self.file_version, "type": self.type,
-            "offset": self.offset, "vectors": vectors,
+            "offset": self.offset, "size": size, "vectors": vectors,
             "annotations": annotations}
