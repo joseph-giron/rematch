8405be051c4cead4ac7a6aa1adaed4d1a4ee5e9c
Nir Izraeli <nirizr@gmail.com>
2017-10-16 17:34:55 +0300

    Adjust celery related requirements
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/rematch/settings/base.py
+++ server/rematch/settings/base.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
-    'djcelery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
