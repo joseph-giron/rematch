e12ca4c64a71a7fbebea990a5f9f3c555bd16b67
Nir Izraeli <nirizr@gmail.com>
2017-11-01 21:17:47 +0200

    Implement step methods
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/collab/strategies/strategy_step.py
+++ server/collab/strategies/strategy_step.py
@@ -1,3 +1,12 @@
+from django.db.models import Q
+
+
 class StrategyStep(object):
   def __init__(self, matcher):
     self.matcher = matcher
+
+  def get_source_filters(self):
+    return Q(type=self.matcher.vector_type)
+
+  def get_target_filters(self):
+    return Q(type=self.matcher.vector_type)
